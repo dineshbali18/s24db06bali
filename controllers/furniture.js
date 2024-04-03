67f18cc8907f53d80bcc34e0d0fea169379e11c2
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2024-04-03 18:52:35 -0500

    added post method

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -8,9 +8,26 @@ exports.furniture_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: furniture detail: ' + req.params.id);
 };
 // Handle furniture create on POST.
-exports.furniture_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: furniture create POST');
-};
+exports.furniture_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new furniture();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.material_type = req.body.material_type;
+    document.price = req.body.price;
+    document.style = req.body.style;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // Handle furniture delete from on DELETE.
 exports.furniture_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: furniture delete DELETE ' + req.params.id);
@@ -25,6 +42,20 @@ exports.furniture_list = async function(req, res) {
     try{
     theFurnitures = await furniture.find();
     res.send(theFurnitures);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+    //VIEWS
+    // Handle a show all view
+    exports.furniture_view_all_Page = async function(req, res) {
+    try{
+    thefurnitures = await furniture.find();
+    console.log('aaaaaaaaaaaaaa',thefurnitures)
+    res.render('furnishing', { title: 'furniture Search Results', results: thefurnitures });
     }
     catch(err){
     res.status(500);
