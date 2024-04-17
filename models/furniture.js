603241b608fc48817491b40d118562526f0f4c7d
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2024-04-17 12:27:23 -0500

    assignment 13

--- models/furniture.js
+++ models/furniture.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose")
 const furnitureSchema = mongoose.Schema({
-material_type: String,
-style: String,
+material_type: {
+    type: String,
+    minlength: 1,
+    maxlength:20,
+},
+style: {
+    type: String,
+    minlength: 1,
+    maxlength:20,
+
+},
 price: Number
 })
 module.exports = mongoose.model("Furniture",
--- routes/furnishing.js
+++ routes/furnishing.js
@@ -1,16 +1,31 @@
 var express = require('express');
+var passport = require('passport')
 const furniture_controllers = require('../controllers/furniture')
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 /* GET home page. */
 router.get('/', furniture_controllers.furniture_view_all_Page);
 
 router.get('/detail', furniture_controllers.furniture_view_one_Page);
 
-router.get('/create', furniture_controllers.furniture_create_Page);
+router.get('/create', secured, furniture_controllers.furniture_create_Page);
 
-router.get('/update', furniture_controllers.furniture_update_Page);
+router.get('/update', secured, furniture_controllers.furniture_update_Page);
 
-router.get('/delete', furniture_controllers.furniture_delete_Page);
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.redirect('/');
+});
+
+
+router.get('/delete', secured, furniture_controllers.furniture_delete_Page);
 
 module.exports = router;
